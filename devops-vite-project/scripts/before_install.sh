aaba5045366121e746cbd3bca997bceebee5230d
Akankshya02 <akankshyapattnaik5@gmail.com>
2025-07-25 01:54:10 +0530

    run again

--- devops-vite-project/scripts/before_install.sh
+++ devops-vite-project/scripts/before_install.sh
@@ -2,3 +2,9 @@
 echo "Running BeforeInstall step..."
 cd /home/ec2-user
 rm -rf app
+sudo yum update -y
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+
